6072f93a48951779682fe24e820af9edd4f5ff87
Lara1011 <laraabuhamad@gmail.com>
2021-12-26 09:21:19 +0200

    Node class

--- implementation/Node.py
+++ implementation/Node.py
@@ -1,33 +1,33 @@
 class Node:
 
     def __init__(self,id,x,y,z):
-        self.id = id;
-        self.x = x;
-        self.y = y;
-        self.z = z;
-        self.in_ = dict();
-        self.out_ = dict();
+        self.id = id
+        self.x = x
+        self.y = y
+        self.z = z
+        self.in_ = dict()
+        self.out_ = dict()
 
     def getId(self):
-        return self.id;
+        return self.id
 
     def setId(self,id):
-        self.id = id;
+        self.id = id
 
     def getx(self):
-        return self.x;
+        return self.x
 
     def setx(self,x):
-        self.x = x;
+        self.x = x
 
     def gety(self):
-        return self.y;
+        return self.y
 
     def sety(self,y):
-        self.y = y;
+        self.y = y
 
     def getz(self):
-        return self.z;
+        return self.z
 
     def setz(self,z):
-        self.z = z;
+        self.z = z
