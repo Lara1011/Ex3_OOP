0af0a1c6bd25a78ee0d0cf935c4cd0b3bd270361
Lara1011 <laraabuhamad@gmail.com>
2021-12-26 09:21:09 +0200

    Edge class

--- implementation/Edge.py
+++ implementation/Edge.py
@@ -1,24 +1,24 @@
 class Edge:
 
     def __init__(self,src,dest,weight):
-        self.src = src;
-        self.dest = dest;
-        self.weight = weight;
+        self.src = src
+        self.dest = dest
+        self.weight = weight
 
     def getSrc(self):
-        return self.src;
+        return self.src
 
     def setSrc(self,src):
-        self.src = src;
+        self.src = src
 
     def getDest(self):
-        return self.dest;
+        return self.dest
 
     def setDest(self,dest):
-        self.dest = dest;
+        self.dest = dest
 
     def getWeight(self):
-        return self.weight;
+        return self.weight
 
     def setWeight(self,weight):
-        self.weight = weight;
+        self.weight = weight
